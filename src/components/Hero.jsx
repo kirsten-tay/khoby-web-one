0b76bdeca1fb6090ea25d82c3b9f052c0376ee91
Kirsten tay <taykirsten11@gmail.com>
2022-04-11 13:22:53 +0000

    updated the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Photo from '../assets/Photo.jpg'
+import React,{Component} from 'react'
 
 const Hero = () => {
   return (
-    
-      <div className="flex ">
-      <img src={"Photo"} className=" absolute  w-full" />
+    <div className="max-w-7x1 mx-auto ">
+
+    <div  className="photo w-full relative">
+    <div className=" absolute  bg-white-600 text-white p-1  rounded-full ">
+              <span className="text-blue-600 text-[20px]">MULTIPURPOSE</span><br/>
+              <span className="font-large">#1 The Fastest</span><br/>
+              <span>Worldpress theme</span>
+               </div>
       </div>
-      
+      </div>
   );
 }
 
