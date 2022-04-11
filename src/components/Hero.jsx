3b1c26b7202b5c6345a472284f9680f780931ef1
Kirsten tay <taykirsten11@gmail.com>
2022-04-11 13:47:48 +0000

    updated the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,15 @@ const Hero = () => {
     <div className="max-w-7x1 mx-auto ">
 
     <div  className="photo w-full relative">
-    <div className=" absolute  bg-white-600 text-white p-1  rounded-full ">
-              <span className="text-blue-600 text-[20px]">MULTIPURPOSE</span><br/>
-              <span className="font-large">#1 The Fastest</span><br/>
-              <span>Worldpress theme</span>
+    <div className="flex flex-col absolute text-left text-black p-1   rounded-full ">
+              <span className="text-blue-600 text-[15px]">MULTIPURPOSE</span>
+              <span className="text-4xl font-medium">#1 The Fastest</span>
+              <span className="text-4xl font-medium">Worldpress theme</span>
+              <span></span>
+              <span/>
+              <span>You can create a website for any business. The template</span>
+              <span>has many different sections and section types from which</span>
+              <span>you can create your own unique website.</span>
                </div>
       </div>
       </div>
