63e21cbc415b21f740e8886c69b2f83234a6e92f
Kirsten tay <taykirsten11@gmail.com>
2022-04-14 00:24:20 +0000

    updated ourworks

--- src/components/Ourworks.jsx
+++ src/components/Ourworks.jsx
@@ -18,17 +18,23 @@ return(
            <span>QA & TESTING</span>
            <span>UX & DESIGN</span>
             </div>
-</div>
+
             <div className="grid grid-col-1 gab-6 my-10 max-w-7xl mx-auto lg:grid-cols-3 gab-6 ">
-            <span  className="items-center p-5"><img src={Photo}/></span>
-            <span  className="items-center p-5"><img src={Photo}/></span>
-            <span  className="items-center p-5"><img src={Photo}/></span>
+            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
+            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
+            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
             <span className="items-center p-5"><img src={Photo}/></span>
             <span  className="items-center p-5"><img src={Photo}/></span>
             <span  className="items-center p-5"><img src={Photo}/></span>
+            </div>
 
+            <div className="p-6 flex justify-center items-center mx-auto">
+                        <button className="flex px-11 py-3  items-center border-o outline-none hover:bg-blue-700 font-medium p-2 bg-blue-600 text-white rounded-md my-3">
+                            All Services
+                            <ArrowNarrowRightIcon className="h-6 w-6 py-1" />
+                        </button>
+                    </div>
 
-            
         </div>
     </div>
     </div>
