49544cd7aa0c7f99bf57a3327b4e66e0da70cca1
Kirsten tay <taykirsten11@gmail.com>
2022-04-14 09:51:47 +0000

    updated ourworks

--- src/components/Ourworks.jsx
+++ src/components/Ourworks.jsx
@@ -20,13 +20,25 @@ return(
            <span>UX & DESIGN</span>
             </div>
 
-            <div className="grid grid-col-1 gab-6 my-10 max-w-7xl mx-auto lg:grid-cols-3 gab-6 ">
-            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
-            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
-            <span  className="items-center p-5 rounded-md"><img src={Photo}/></span>
-            <span className="items-center p-5"><img src={Photo}/></span>
-            <span  className="items-center p-5"><img src={Photo}/></span>
-            <span  className="items-center p-5"><img src={Photo}/></span>
+            <div className="grid grid-col-1 gab-6 my-10 max-w-9xl mx-auto lg:grid-cols-3 gab-6 ">
+            <span  className="items-center p-5 rounded-md ">
+                <img src={Photo}/>
+                </span>
+            <span  className="items-center p-5 rounded-md ">
+                <img src={Photo}/>
+                </span>
+            <span  className="items-center p-5 rounded-md ">
+                <img src={Photo}/>
+                </span>
+            <span className="items-center p-5">
+                <img src={Photo}/>
+                </span>
+            <span  className="items-center p-5">
+                <img src={Photo}/>
+                </span>
+            <span  className="items-center p-5">
+                <img src={Photo}/>
+                </span>
             </div>
 
             <div className="p-6 flex justify-center items-center mx-auto">
