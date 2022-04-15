3290e647d102ab0275faa8335c342e448a459a4f
Kirsten tay <taykirsten11@gmail.com>
2022-04-15 22:39:08 +0000

    updated whoweare

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -52,6 +52,17 @@ const Whoweare =( ) => {
 
                 <div>
                  <h5 className="text-blue-500">REVIEWS FROM OUR CLIENTS</h5>
+                <h2 className="font-medium text-5xl">What People Say</h2>
+                </div>
+
+                <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto space-x-7 lg:grid-cols-4 gab-6 ">
+                    <div className="border-2 p-5 rounded-md">
+                     <span>Catherine Williams</span>
+                     <span>Regular client</span>
+                     <span>PathSoft offers a high caliber of resources 
+                         skilled in Microsoft Azure.NET, mobile and Quality Assurance. 
+                         They became our true business partners over the past three years.</span>
+                    </div>
                 </div>
             
         </div>
