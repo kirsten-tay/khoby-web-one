c91fbcd0745dba3f162204135b78c9b0ede58369
Kirsten tay <taykirsten11@gmail.com>
2022-04-15 22:29:31 +0000

    updated whoweare

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -12,8 +12,35 @@ const Whoweare =( ) => {
                 WHO WE ARE</h5>
                 <h2 className="font-medium text-4xl">Meet The Team</h2>
 
-                <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto lg:grid-cols-3 gab-6 ">
-            <div className="border-2 rounded-md">
+                <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto space-x-7 lg:grid-cols-4 gab-6 ">
+            <div className="border-2  rounded-md">
+            <span className="items-center  ">
+                <img src={Photo} />
+                <span className=" text-2xl font-medium">
+                Mary Lee
+                </span><br/>
+                <span>software Developer</span>
+                </span>
+                </div>
+
+                <div className="border-2 rounded-md">
+            <span  className="items-center   ">
+                <img src={Photo} />
+                <span className=" text-2xl font-medium">
+                Ryan Wilson                </span><br/>
+                <span>CEO</span>
+                </span></div>
+
+                <div className="border-2 rounded-md">
+            <span  className="items-center  ">
+                <img src={Photo} />
+                <span className=" text-2xl font-medium">
+                Sam Robinson
+               </span><br/>
+                <span>Senior Developer</span>
+                </span></div>
+
+                <div className="border-2 rounded-md">
             <span  className="items-center  ">
                 <img src={Photo} />
                 </span></div>
