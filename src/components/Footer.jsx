29d3726039c51654bfb439aa48df88f986a534d8
Kirsten tay <taykirsten11@gmail.com>
2022-04-17 22:32:05 +0000

    added a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,20 @@ import React from 'react'
 const Footer= ()=> {
 return(
     <div>
+ 
+      <div>
+      <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto space-x-7 p-5 lg:grid-cols-3 gab-6 ">
+           
+      <div className="flex flex-1">
+            <img src={'https://pathsoft.kovalweb.com/full/wp-content/uploads/2021/06/logo.svg'} alt={'top'} className="w-90 " />
+          </div>
 
+           <div>
+            <span>Menu 1</span>
+            <span>About Us</span>
+           </div>
+         </div>
+      </div>
     </div>
 )
 
