0dcd3d680a45e668853f25316be6b54b46c2b92c
Kirsten tay <taykirsten11@gmail.com>
2022-04-18 13:31:17 +0000

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,15 @@ return(
       <div className="bg-slate-600 p-10">
       <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto space-x-7 p-5 lg:grid-cols-3 gab-6 ">
            
-      <div className="flex flex-1">
+      <div >
             <img src={'https://pathsoft.kovalweb.com/full/wp-content/uploads/2021/06/logo.svg'} alt={'top'} className="w-90 " />
           </div>
 
-           <div className="col-1">
+           <div className=" flex flex-col">
             <span>Menu 1</span>
             <span>About Us</span>
+            <span>Services</span>
+            <span>Pricing</span>
            </div>
          </div>
       </div>
