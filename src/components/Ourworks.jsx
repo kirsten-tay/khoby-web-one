0e4c203e2be2755a11dd764e19047f3e07954715
Kirsten tay <taykirsten11@gmail.com>
2022-04-19 17:13:24 +0000

    updated the page

--- src/components/Ourworks.jsx
+++ src/components/Ourworks.jsx
@@ -11,58 +11,44 @@ import Photo7 from '../assets/Photo7.jpg'
 
 import { ArrowNarrowRightIcon } from '@heroicons/react/outline';
 
-const Ourworks =() =>{
-return(
-     
-    <div>
-        <div className=" bg-slate-100 p-10 ">
+const Ourworks = () => {
+    return (
+
+        <div>
+            <div className=" bg-slate-100 p-10 ">
                 <div className="p-5">
-            <h5 className="text-blue-600 text-2xl">Our works</h5>
-            <h2 className="font-medium text-5xl">Latest Projects</h2>
-        </div>
-        <div className="w-full">
-        <div className="grid grid-col-1  my-10 max-w-7xl mx-auto lg:grid-cols-5 ">
-           <span>ALL</span>
-           <span>CUSTOM SOFTWARE</span>
-           <span>MOBILE APPS</span>
-           <span>QA & TESTING</span>
-           <span>UX & DESIGN</span>
-            </div>
-
-            <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto lg:grid-cols-3 gab-6  relative">
-            <span  className="items-center p-5 rounded-md ">
-                <img src={Photo2}/>
-                </span>
-            <span  className="items-center p-5 rounded-md ">
-                <img src={Photo3}/>
-                </span>
-            <span  className="items-center p-5 rounded-md ">
-                <img src={Photo4}/>
-                </span>
-            <span className="items-center p-5">
-                <img src={Photo5}/>
-                </span>
-            <span  className="items-center p-5">
-                <img src={Photo6}/>
-                </span>
-            <span  className="items-center p-5">
-                <img src={Photo7}/>
-                </span>
-            </div>
-
-            <div className=" flex justify-center items-center mx-auto">
-                        <button className="flex px-11 py-3  items-center border-o outline-none hover:bg-blue-700 font-medium p-2 bg-blue-600 text-white rounded-md my-3">
-                            All Projects
-                            <ArrowNarrowRightIcon className="h-6 w-6 py-1" />
-                        </button>
+                    <h5 className="text-blue-600 text-2xl">Our works</h5>
+                    <h2 className="font-medium text-5xl">Latest Projects</h2>
+                </div>
+                <div className="w-full">
+                    <div className="flex flex-row space-x-5 justify-center my-10 max-w-7xl mx-auto ">
+                        <h5>ALL</h5>
+                        <h5>CUSTOM SOFTWARE</h5>
+                        <h5>MOBILE APPS</h5>
+                        <h5>QA & TESTING</h5>
+                        <h5>UX & DESIGN</h5>
                     </div>
 
-        </div>
-    </div>
-    </div>
+                    <div className="grid grid-col-1 gab-6  max-w-9xl mx-auto lg:grid-cols-3 gab-6  relative">
+                        <div className="items-center p-5 rounded-md ">
+                            <img src={Photo2} className=" md-w-32 " /> 
+                            <div className="absolute "></div>
+                        </div>
 
-);
+
+                        <div className=" flex justify-center items-center mx-auto">
+                            <button className="flex px-11 py-3  items-center border-o outline-none hover:bg-blue-700 font-medium p-2 bg-blue-600 text-white rounded-md my-3">
+                                All Projects
+                                <ArrowNarrowRightIcon className="h-6 w-6 py-1" />
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            );
 
 }
 
-export default Ourworks;
+            export default Ourworks;
--- src/components/Whychooseus.jsx
+++ src/components/Whychooseus.jsx
@@ -35,8 +35,8 @@ const Whychooseus = () => {
                                     <hr className="w-11 mx-2 h-0.5 bg-gray-600" />
                                 </div>
                                 <div className="flex flex-col text-left space-y-3 items-start">
-                                    <h2 className="font-bold text-xl"> High Quality Hardware</h2>
-                                    <h5 className="text-sm">We use top-notch hardware to develop the most efficient apps for our customers</h5>
+                                    <h2 className="font-bold text-xl"> Dedicated 24\7 Support</h2>
+                                    <h5 className="text-sm">You can rely on our 24/7 tech support that will gladly solve any app issue you may have.</h5>
                                 </div>
                             </div>
                         </div>
@@ -47,8 +47,8 @@ const Whychooseus = () => {
                                     <hr className="w-11 mx-2 h-0.5 bg-gray-600" />
                                 </div>
                                 <div className="flex flex-col text-left space-y-3 items-start">
-                                    <h2 className="font-bold text-xl"> High Quality Hardware</h2>
-                                    <h5 className="text-sm">We use top-notch hardware to develop the most efficient apps for our customers</h5>
+                                    <h2 className="font-bold text-xl"> 30-Day Money-back Guarantee</h2>
+                                    <h5 className="text-sm">If you are not satisfied with our apps, we will return your money in the first 30 days.</h5>
                                 </div>
                             </div>
                         </div>
@@ -59,8 +59,8 @@ const Whychooseus = () => {
                                     <hr className="w-11 mx-2 h-0.5 bg-gray-600" />
                                 </div>
                                 <div className="flex flex-col text-left space-y-3 items-start">
-                                    <h2 className="font-bold text-xl"> High Quality Hardware</h2>
-                                    <h5 className="text-sm">We use top-notch hardware to develop the most efficient apps for our customers</h5>
+                                    <h2 className="font-bold text-xl"> Agile and Fast Working Style</h2>
+                                    <h5 className="text-sm">This type of approach to our work helps our specialists to quickly develop better apps.</h5>
                                 </div>
                             </div>
                         </div>
@@ -71,8 +71,8 @@ const Whychooseus = () => {
                                     <hr className="w-11 mx-2 h-0.5 bg-gray-600" />
                                 </div>
                                 <div className="flex flex-col text-left space-y-3 items-start">
-                                    <h2 className="font-bold text-xl"> High Quality Hardware</h2>
-                                    <h5 className="text-sm">We use top-notch hardware to develop the most efficient apps for our customers</h5>
+                                    <h2 className="font-bold text-xl"> Some Apps are Free</h2>
+                                    <h5 className="text-sm">We also develop free apps that can be downloaded online without any payments.</h5>
                                 </div>
                             </div>
                         </div>
@@ -83,8 +83,8 @@ const Whychooseus = () => {
                                     <hr className="w-11 mx-2 h-0.5 bg-gray-600" />
                                 </div>
                                 <div className="flex flex-col text-left space-y-3 items-start">
-                                    <h2 className="font-bold text-xl"> High Quality Hardware</h2>
-                                    <h5 className="text-sm">We use top-notch hardware to develop the most efficient apps for our customers</h5>
+                                    <h2 className="font-bold text-xl"> High Level of Usability</h2>
+                                    <h5 className="text-sm">All our products have high usability allowing users to easily operate the apps.</h5>
                                 </div>
                             </div>
                         </div>
@@ -101,7 +101,7 @@ const Whychooseus = () => {
                     <div className="absolute rounded-full -top-20 h-80 w-80 -left-[-230px] bg-blue-500  p-28 mx-auto">
                     </div>
 
-                    <div className="absolute rounded-full shadow-b shadow-pink-600 h-80 w-80  -top-20 -left-[-300px] bg-slate-500  p-10 mx-auto">
+                    <div className="absolute rounded-full shadow shadow-slate-900 h-80 w-80  -top-20 -left-[-300px] bg-white  p-10 mx-auto">
                         <h1 className="text-9xl  font-medium text-blue-600"> 10 </h1>
                         <p>years of experience</p>
                     </div>
